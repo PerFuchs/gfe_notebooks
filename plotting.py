09d9f37960c6935550e009b2970e694e104b2b62
Per Fuchs <per.fuchs@gmail.com>
2021-07-21 10:48:56 +0200

    Adjust width for 3 column figures.

--- plotting.py
+++ plotting.py
@@ -194,10 +194,15 @@ def latexify(fig_width=None, fig_height=None, columns=1):
     # Width and max height in inches for IEEE journals taken from
     # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
 
-    assert (columns in [1, 2])
+    assert (columns in [1, 2, 3])
 
     if fig_width is None:
-        fig_width = 3.39 if columns == 1 else 6.9  # width in inches
+        if columns == 1:
+            fig_width = 6.9
+        elif columns == 2:
+            fig_width = 3.39
+        else:
+            fig_width = 2.28
 
     if fig_height is None:
         golden_mean = (np.sqrt(5) - 1.0) / 2.0  # Aesthetic ratio
